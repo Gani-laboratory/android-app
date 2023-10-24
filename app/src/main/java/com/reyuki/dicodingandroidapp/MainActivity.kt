0608067d56654d43b536b237fa33d434ebe21152
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-24 16:53:48 +0700

    add viewbinding and other property/attribute

--- app/src/main/java/com/reyuki/dicodingandroidapp/MainActivity.kt
+++ app/src/main/java/com/reyuki/dicodingandroidapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.reyuki.dicodingandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.reyuki.dicodingandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvArticles: RecyclerView
+    private val list = ArrayList<Article>()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
